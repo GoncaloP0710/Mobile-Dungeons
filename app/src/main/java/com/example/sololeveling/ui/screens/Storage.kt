7953b86eb888d2fd8934e863e5aec51f9a8c08cb
André Reis <bakugan.erdna@gmail.com>
2025-01-19 16:39:20 +0000

    profile edit broken

--- app/src/main/java/com/example/sololeveling/ui/screens/Storage.kt
+++ app/src/main/java/com/example/sololeveling/ui/screens/Storage.kt
@@ -37,7 +37,11 @@ import com.example.sololeveling.R
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.getValue
 import android.content.Context
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 
 @Composable
 fun Storage(
@@ -50,6 +54,7 @@ fun Storage(
 
     var userName2 =userName
     // Use state to hold values
+    var firstTime by remember { mutableStateOf(0) }
     var age by remember { mutableStateOf(0) }
     var description by remember { mutableStateOf("") }
     var dungeonsSpotted by remember { mutableStateOf(0) }
@@ -58,24 +63,31 @@ fun Storage(
     var wait by remember { mutableStateOf(true) }
     var scanning by remember { mutableStateOf(false) }
     var magneticField by remember { mutableDoubleStateOf(0.0) }
+    var edit by remember { mutableStateOf(false) }
+    var age2 by remember { mutableStateOf(age.toString()) }
+    var dungeonsSpotted2 by remember { mutableStateOf(dungeonsSpotted.toString()) }
 
     val usersRef = db.getReference("UsersInfo")
 
-    // Fetch data from Firebase
-    LaunchedEffect(userName) { // Ensures this runs only once for the user
-        usersRef.child(userName).get().addOnSuccessListener { snapshot ->
-            if (snapshot.exists()) {
-                age = snapshot.child("Age").getValue<Int>() ?: 0
-                description = snapshot.child("Description").getValue<String>().orEmpty()
-                dungeonsSpotted = snapshot.child("DungeonsSpotted").getValue<Int>() ?: 0
-                powerLevel = snapshot.child("PowerLevel").getValue<String>().orEmpty()
-            } else {
-                println("User not found.")
+    if(firstTime == 0){
+        // Fetch data from Firebase
+        LaunchedEffect(userName) { // Ensures this runs only once for the user
+            usersRef.child(userName).get().addOnSuccessListener { snapshot ->
+                if (snapshot.exists()) {
+                    age = snapshot.child("Age").getValue<Int>() ?: 0
+                    description = snapshot.child("Description").getValue<String>().orEmpty()
+                    dungeonsSpotted = snapshot.child("DungeonsSpotted").getValue<Int>() ?: 0
+                    powerLevel = snapshot.child("PowerLevel").getValue<String>().orEmpty()
+                } else {
+                    println("User not found.")
+                }
+            }.addOnFailureListener {
+                println("Error: ${it.message}")
             }
-        }.addOnFailureListener {
-            println("Error: ${it.message}")
         }
+        firstTime +=1
     }
+
 
     // --------------------------------------------------------------------------------------
 
@@ -114,123 +126,203 @@ fun Storage(
 
             Spacer(modifier = Modifier.height(40.dp)) // Space between image and text
 
-            // Place text below the image
-            Text(
-                text = "Name: $userName",
-                fontSize = 30.sp,
-                textAlign = TextAlign.Center,
-            )
-            Spacer(modifier = Modifier.height(8.dp)) // Adjusted spacing
-            Text(
-                text = "Age: $age",
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center,
-            )
-            Spacer(modifier = Modifier.height(1.dp)) // Adjusted spacing
-            Text(
-                text = "Description: $description",
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center,
-            )
-            Spacer(modifier = Modifier.height(1.dp)) // Adjusted spacing
-            Text(
-                text = "Dungeons Spotted: $dungeonsSpotted",
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center,
-            )
-            Spacer(modifier = Modifier.height(1.dp)) // Adjusted spacing
-            PowerLevel(powerLevel)
-            Spacer(modifier = Modifier.height(2.dp)) // Adjusted spacing
-
-            if(wait && scanning){
-                Text(scanning_string)
-            }else if(!wait){
-                measureMagneticField(context) { magneticField = it }
-                when(magneticField){
-                    in 0.0 .. 33.0 -> Text(text="E rank",
-                        style = TextStyle(
-                            fontSize = 30.sp,
-                            fontWeight = FontWeight.Bold,
-                            brush = Brush.linearGradient(
-                                colors = listOf(Color(0xFF808080), Color(0xFFB0C4DE)) // Gradient colors
-                            ))
-                    )
-
-                    in 33.0 .. 66.0 -> Text(text="D rank",
-                        style = TextStyle(
-                            fontSize = 30.sp,
-                            fontWeight = FontWeight.Bold,
-                            brush = Brush.linearGradient(
-                                colors = listOf(Color(0xFF1E90FF), Color(0xFF808080)) // Gradient colors
-                            ))
-                    )
-
-                    in 66.0 .. 100.0 -> Text(text="C rank",
-                        style = TextStyle(
-                            fontSize = 30.sp,
-                            fontWeight = FontWeight.Bold,
-                            brush = Brush.linearGradient(
-                                colors = listOf(Color(0xFF32CD32), Color(0xFF1E90FF)) // Gradient colors
-                            ))
-                    )
-
-                    in 100.0 .. 133.0 -> Text(text="B rank",
-                        style = TextStyle(
-                            fontSize = 30.sp,
-                            fontWeight = FontWeight.Bold,
-                            brush = Brush.linearGradient(
-                                colors = listOf(Color(0xFFFFD700), Color(0xFF32CD32)) // Gradient colors
-                            ))
-                    )
-
-                    in 133.0 .. 166.0 -> Text(text="A rank",
-                        style = TextStyle(
-                            fontSize = 30.sp,
-                            fontWeight = FontWeight.Bold,
-                            brush = Brush.linearGradient(
-                                colors = listOf(Color(0xFFFFA500), Color(0xFFFFD700)) // Gradient colors
-                            ))
-                    )
-
-                    in 166.0 .. 200.0 -> Text(text="S rank",
-                        style = TextStyle(
-                            fontSize = 30.sp,
-                            fontWeight = FontWeight.Bold,
-                            brush = Brush.linearGradient(
-                                colors = listOf(Color(0xFFFFD700), Color(0xFFFF4500)) // Gradient colors
-                            ))
-                    )
-
-
-                    else -> Text("Erro: + $magneticField")
-                }
-
+            if(edit){
+                OutlinedTextField(
+                    value = userName2,
+                    onValueChange = { userName2 = it },
+                    label = { Text("Name", color = Color.White) },
+                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                    textStyle = TextStyle(color = Color.White),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.White,
+                        cursorColor = Color.White // Optionally, change the cursor color
+                    ),
+                )
+            }else{
+                // Place text below the image
+                Text(
+                    text = "Name: $userName2",
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                )
             }
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = {
-                val timer = object: CountDownTimer(5000, 500) {
-                    override fun onTick(millisUntilFinished: Long) {
-                        when(scanning_string){
-                            "Scanning...." -> scanning_string = "Scanning"
-                            "Scanning..." -> scanning_string = "Scanning...."
-                            "Scanning.." -> scanning_string = "Scanning..."
-                            "Scanning." -> scanning_string = "Scanning.."
-                            "Scanning" -> scanning_string = "Scanning."
+
+            Spacer(modifier = Modifier.height(8.dp)) // Adjusted spacing
+            if(edit){
+                OutlinedTextField(
+                    value = age2,
+                    onValueChange = { age2 = it },
+                    label = { Text("Age", color = Color.White) },
+                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                    textStyle = TextStyle(color = Color.White),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.White,
+                        cursorColor = Color.White // Optionally, change the cursor color
+                    ),
+                )
+                age = age2.toInt()
+            }else{
+                Text(
+                    text = "Age: $age",
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                )
+            }
+
+            Spacer(modifier = Modifier.height(1.dp)) // Adjusted spacing
+            if(edit){
+                OutlinedTextField(
+                    value = description,
+                    onValueChange = { description = it },
+                    label = { Text("Description", color = Color.White) },
+                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                    textStyle = TextStyle(color = Color.White),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.White,
+                        cursorColor = Color.White // Optionally, change the cursor color
+                    ),
+                )
+            }else{
+                Text(
+                    text = "Description: $description",
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                )
+            }
+
+            Spacer(modifier = Modifier.height(1.dp)) // Adjusted spacing
+            if(edit){
+                OutlinedTextField(
+                    value = dungeonsSpotted2,
+                    onValueChange = { dungeonsSpotted2 = it },
+                    label = { Text("Dungeons Spotted", color = Color.White) },
+                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                    textStyle = TextStyle(color = Color.White),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.White,
+                        cursorColor = Color.White // Optionally, change the cursor color
+                    ),
+                )
+            }else{
+                Text(
+                    text = "Dungeons Spotted: $dungeonsSpotted",
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                )
+            }
+            if(!edit){
+                Spacer(modifier = Modifier.height(1.dp)) // Adjusted spacing
+                PowerLevel(powerLevel)
+                Spacer(modifier = Modifier.height(2.dp)) // Adjusted spacing
+
+                if(wait && scanning){
+                    Text(scanning_string)
+                }else if(!wait){
+                    measureMagneticField(context) { magneticField = it }
+                    when(magneticField){
+                        in 0.0 .. 33.0 -> Text(text="E rank",
+                            style = TextStyle(
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.Bold,
+                                brush = Brush.linearGradient(
+                                    colors = listOf(Color(0xFF808080), Color(0xFFB0C4DE)) // Gradient colors
+                                ))
+                        )
+
+                        in 33.0 .. 66.0 -> Text(text="D rank",
+                            style = TextStyle(
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.Bold,
+                                brush = Brush.linearGradient(
+                                    colors = listOf(Color(0xFF1E90FF), Color(0xFF808080)) // Gradient colors
+                                ))
+                        )
+
+                        in 66.0 .. 100.0 -> Text(text="C rank",
+                            style = TextStyle(
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.Bold,
+                                brush = Brush.linearGradient(
+                                    colors = listOf(Color(0xFF32CD32), Color(0xFF1E90FF)) // Gradient colors
+                                ))
+                        )
+
+                        in 100.0 .. 133.0 -> Text(text="B rank",
+                            style = TextStyle(
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.Bold,
+                                brush = Brush.linearGradient(
+                                    colors = listOf(Color(0xFFFFD700), Color(0xFF32CD32)) // Gradient colors
+                                ))
+                        )
+
+                        in 133.0 .. 166.0 -> Text(text="A rank",
+                            style = TextStyle(
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.Bold,
+                                brush = Brush.linearGradient(
+                                    colors = listOf(Color(0xFFFFA500), Color(0xFFFFD700)) // Gradient colors
+                                ))
+                        )
+
+                        in 166.0 .. 200.0 -> Text(text="S rank",
+                            style = TextStyle(
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.Bold,
+                                brush = Brush.linearGradient(
+                                    colors = listOf(Color(0xFFFFD700), Color(0xFFFF4500)) // Gradient colors
+                                ))
+                        )
+
+
+                        else -> Text("Erro: + $magneticField")
+                    }
+
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = {
+                    val timer = object: CountDownTimer(5000, 500) {
+                        override fun onTick(millisUntilFinished: Long) {
+                            when(scanning_string){
+                                "Scanning...." -> scanning_string = "Scanning"
+                                "Scanning..." -> scanning_string = "Scanning...."
+                                "Scanning.." -> scanning_string = "Scanning..."
+                                "Scanning." -> scanning_string = "Scanning.."
+                                "Scanning" -> scanning_string = "Scanning."
+                            }
+                        }
+
+                        override fun onFinish() {
+                            wait = false
+                            scanning = false
                         }
                     }
+                    scanning = true
+                    wait = true
+                    timer.start()
 
-                    override fun onFinish() {
-                        wait = false
-                        scanning = false
-                    }
+
+                }){
+                    Text("Measure Power")
                 }
-                scanning = true
-                wait = true
-                timer.start()
+                Spacer(modifier = Modifier.height(16.dp))
 
 
-            }){
-                Text("Measure Power")
+
+
+            }
+            if(edit){
+                Button(onClick = {edit = false}) {
+                    Text("Save")
+                }
+            }else{
+                Button(onClick = {edit = true}) {
+                    Text("Edit Profile")
+                }
             }
         }
     }
